d788ca87d7aaa9d7a977a061de169f1d4c654467
rafaelminatto1 <58711430+rafaelminatto1@users.noreply.github.com>
2020-05-03 20:41:52 -0300

    Add files via upload

---
+++ precache-manifest.73bd21fe49656ed88ffbf2a104981fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdc25c6645f4b61b4c34fcf65d6c74f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7872c28fbbf9eb5976d",
+    "url": "/static/css/main.c0993e47.chunk.css"
+  },
+  {
+    "revision": "acb908cb3d7ab96232a3",
+    "url": "/static/js/2.2e704cfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e704cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7872c28fbbf9eb5976d",
+    "url": "/static/js/main.0d58980a.chunk.js"
+  },
+  {
+    "revision": "89ac0ceab379482007a8",
+    "url": "/static/js/runtime-main.3005b24e.js"
+  }
+]);
