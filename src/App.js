f0dde93c5a6afb6379741021138afdfe5e852162
minattorafael <58148065+minattorafael@users.noreply.github.com>
2022-12-23 08:56:03 -0300

    Update api
    Used another API because the old link was broken

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import MonitorItem from"./componets/monitoritem";
 
 const getData= async() =>{
- const response =await axios.get("https://coronavirus-19-api.herokuapp.com/countries/brazil"
+ const response =await axios.get("https://covid19-brazil-api.now.sh/api/report/v1/brazil"
  );
 console.log("response", response);
 }
@@ -17,18 +17,18 @@ function App() {
       getData()}
       , 2000);
     },[]);
-const [casos, setCasos] = useState();
-const[casosHoje, setCasosHoje] =useState();
+const[country, setCasosHoje] =useState();    
+const [confirmed, setConfirmed] = useState();
 const [mortes, setMortes] = useState();
-const[critical, setCritical] =useState();
+const[updated_at, setCritical] =useState();
 const getData= async() =>{
-  const response =await axios.get("https://coronavirus-19-api.herokuapp.com/countries/brazil"
+  const response =await axios.get("https://covid19-brazil-api.now.sh/api/report/v1/brazil"
   );
  console.log("response", response.data);
- setCasos(response.data.cases)
- setCasosHoje(response.data.todayCases)
- setMortes(response.data.deaths)
- setCritical(response.data.critical)
+ setCasosHoje(response.data.data.country)
+ setConfirmed(response.data.data.confirmed)
+  setMortes(response.data.data.deaths)
+ setCritical(response.data.data.updated_at)
  };
   return (
     <div className="App">
@@ -36,14 +36,13 @@ const getData= async() =>{
       
         <h1>
           Monitor Covid-19
+          
         </h1>
         <div style={{display: "flex", justifyContent: "space-around", width:"100%", flexDirection: "row"}}>
-        <MonitorItem label ="Casos totais" number={casos}/>
-        <MonitorItem label ="Casos Hoje"
-        number={casosHoje}
-        increase={((casosHoje/(casos-casosHoje))*  100).toFixed(2)} />
-        <MonitorItem label ="Mortes" number={mortes}/>
-        <MonitorItem label ="Casos Críticos" number={critical}/>
+        <MonitorItem label ="País" number={country}  />
+        <MonitorItem label ="Casos totais" number={confirmed}/>
+                <MonitorItem label ="Mortes" number={mortes}/>
+        <MonitorItem label ="Atualizdo às" number={updated_at}/>
         
         </div>
         
